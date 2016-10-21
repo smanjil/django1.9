cba06f034770c7128a0df8869442b01149ed7b19
Manjil Shrestha <manjil.shrestha@yipl.com.np>
2016-10-21 07:51:42 +0545

    User Authentication - ep(36)

--- music/urls.py
+++ music/urls.py
@@ -6,11 +6,23 @@ app_name = 'music'
 
 urlpatterns = [
     # /music/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+
+    # register user
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
+
+    # logout
+    url(r'^logout/$', views.logoutUser, name='logout'),
 
     # /music/71/    
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
-    # /music/71/favorite/
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    # /music/album/add/
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+    # /music/album/2/
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+
+    # /music/album/2/delete/
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
 ]
